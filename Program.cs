8d56fffdd1cc1755d928205a5a7398e3dd5d9688
Benjamin Chen <Benjamin.Chen@lntinfotech.com>
2021-09-20 16:54:05 -0400

    Refactored to use switch cases

--- Program.cs
+++ Program.cs
@@ -14,31 +14,24 @@ namespace Roman_Numeral_Converter
                     case 'M':
                         total += 1000;
                         break;
-
                     case 'D':
                         total += 500;
                         break;
-
                     case 'C':
                         total += 100;
                         break;
-
                     case 'L':
                         total += 50;
                         break;
-
                     case 'X':
                         total += 10;
                         break;
-
                     case 'V':
                         total += 5;
                         break;
-
                     case 'I':
                         total += 1;
                         break;
-
                 }
             }
 
